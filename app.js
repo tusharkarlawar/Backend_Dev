bffdd9beafba16483284f3d7e5462e2e09cbd94a
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-20 16:48:32 +0530

    Routing_Request / Response

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
-    //log your name to the console
-    const myName = "Tush"; //Relpace your name 
-    console.log('my name is :', myName);
-
-    //send your name in the response to the browser
-    res.end(myName);
-
+     //const url = req.url;
+    if (req.url === "/") {
+        res.end("Welcome home");
+    }else if ( req.url === "/about") {
+        res.end("Welcome to About Us page");
+    }else if (req.url === "/node") {
+        res.end("Welcome to my Node js project");
+    } else {
+        res.end("Invalid URL");
+    }
 });
-server.listen(4000);
+server.listen(7000);
