df15b5f20998b5d7a1c1d4b63b9def3468a4cefb
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-23 22:01:51 +0530

    Middlewares

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 
 const express = require('express');
-
+ const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res,next) =>{
-    console.log('in the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) =>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use((req, res,next) =>{
-    console.log('in another the middleware');
+app.post('/product',(req, res,next) =>{
+    console.log(req.body);
+    res.redirect('<h1>Hello from Express!</h1>');
+});
+
+app.use('/',(req, res,next) =>{
     res.send('<h1>Hello from Express!</h1>');
 });
-
    
 app.listen(3004);
