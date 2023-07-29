5bc0f6b9e49babfe6c14be4509317bbf325595c6
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-29 12:16:10 +0530

    MySQL-NodeJs

---
+++ Delete_Product/util/database.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node_complete',
+    password: 'Tushar@9921'
+});
+
+module.export = pool.promise();
+
